b7378f88f21b1f469388c83310cb8b480d0b42fa
7rans <transfire@gmail.com>
2015-03-28 13:46:10 -0400

    Work around JRuby new line issue. :test:

--- demo/applique/examples.rb
+++ demo/applique/examples.rb
@@ -13,6 +13,8 @@ When /The result will be/ do |text|
   b = binding
   Kernel.eval(@code, b)
   result = RagTag.compile(@template, b).to_xhtml
+  # IMPORTANT! This is only here b/c JRuby causes the insertion of new lines.
+  result = result.delete("\n")
   text.strip.assert == result.strip
 end
 
